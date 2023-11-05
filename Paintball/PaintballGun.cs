474cc6eb047b8020074204a8b9c2797c425a837e
Durrell Jules <dujules23@gmail.com>
2023-11-04 22:15:59 -0400

    created Main method for Paintball, added PaintballGun class, works as intended

---
+++ Paintball/PaintballGun.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Application
+{
+	public class PaintballGun
+	{
+		public PaintballGun()
+		{
+		}
+
+        public const int MAGAZINE_SIZE = 16;
+
+        private int balls = 0;
+        private int ballsLoaded = 0;
+
+        public int GetBallsLoaded() { return ballsLoaded;}
+
+        public bool IsEmpty() { return ballsLoaded == 0; }
+
+        public int GetBalls() { return balls; }
+
+        public void SetBalls(int numberOfBalls)
+        {
+            if (numberOfBalls > 0)
+                balls = numberOfBalls;
+            Reload();
+        }
+
+        public void Reload()
+        {
+            if (balls > MAGAZINE_SIZE)
+                ballsLoaded = MAGAZINE_SIZE;
+            else
+                ballsLoaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if (ballsLoaded == 0) return false;
+            ballsLoaded--;
+            balls--;
+            return true;
+        }
+	}
+}
+
